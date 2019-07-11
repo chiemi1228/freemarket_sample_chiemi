88b33929e7699db542219bacdf9a2bb5385c2d08
chiemi1228 <sucreunpeu@yahoo.co.jp>
2019-07-11 21:56:18 +0900

    Update schema.rb
    "name"に  default: ""　追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711081416) do
+ActiveRecord::Schema.define(version: 20190711125134) do
 
   create_table "buys", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20190711081416) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",                   default: ""
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
